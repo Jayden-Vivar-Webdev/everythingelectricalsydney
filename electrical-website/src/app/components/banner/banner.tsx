e25e548d4d79325ade24521fa3862629ec7ed079
Jayden-Vivar-Webdev <jaydenjv@hotmail.com>
2025-07-17 13:34:38 +1000

    Update banner.tsx

--- electrical-website/src/app/components/banner/banner.tsx
+++ electrical-website/src/app/components/banner/banner.tsx
@@ -3,6 +3,7 @@ export default function Banner() {
     <div className="flex items-center gap-x-6 secondary-bg px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <p className="text-sm/6 text-white">
         <a href="tel:0449003526">
+          <div className="w-3 h-3 bg-green-500 rounded-full"></div>
           <strong className="font-semibold">We’re available 24/7</strong>
           <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline size-0.5 fill-current">
             <circle r={1} cx={1} cy={1} />
