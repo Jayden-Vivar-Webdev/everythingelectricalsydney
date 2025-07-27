33ba2838be25eee4190fb5a88aff7a4bcfc0082f
Jayden-Vivar-Webdev <jaydenjv@hotmail.com>
2025-07-28 09:30:24 +1000

    Update layout.tsx

--- electrical-website/src/app/layout.tsx
+++ electrical-website/src/app/layout.tsx
@@ -4,6 +4,7 @@ import '@/app/globals.css'
 import FooterBanner from "./components/banner/nexa-banner";
 import { Analytics } from "@vercel/analytics/next"
 // import Footer from "./components/footer/footer";
+import Head from 'next/head';
 import NavBarTemp from "./components/navbar/temp";
 
 export const metadata = {
@@ -63,10 +64,32 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MP2GJLD');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </Head>
       <body
         
       >
-        {/* <NavBar /> */}
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MP2GJLD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <NavBarTemp/>
         <main className="pt-[6rem]">
         {/*<main className=""> */}
