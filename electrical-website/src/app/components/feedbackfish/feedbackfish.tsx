ea365f2ebed18e19a36ea6b3a4f12d44780ca910
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-07-28 12:13:22 +1000

    added site

--- electrical-website/src/app/components/feedbackfish/feedbackfish.tsx
+++ electrical-website/src/app/components/feedbackfish/feedbackfish.tsx
@@ -4,12 +4,13 @@ import { FeedbackFish } from '@feedback-fish/react';
 
 export default function FeedbackWidget({ userEmail }: { userEmail?: string }) {
   return (
-    <div className="fixed bottom-1/8 right-6 z-50">
-      <FeedbackFish projectId="aef555de2af14e" userId={userEmail}>
+    <div className="fixed bottom-15 left-1/2 z-50 transform -translate-x-1/2 scale-80">
+    <FeedbackFish projectId="aef555de2af14e" userId={userEmail}>
         <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-full shadow-lg transition">
-          💬 Feedback
+        💬 Feedback
         </button>
-      </FeedbackFish>
+    </FeedbackFish>
     </div>
+
   );
 }
