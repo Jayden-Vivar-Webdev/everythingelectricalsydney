8f4450b8fbb7d385cd31401082e76c61a4f8a5cb
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2025-07-28 12:41:23 +1000

    added site

--- electrical-website/src/app/components/feedbackfish/feedbackfish.tsx
+++ electrical-website/src/app/components/feedbackfish/feedbackfish.tsx
@@ -1,23 +1,8 @@
 'use client';
-
-import { useEffect } from 'react';
 import { FeedbackFish } from '@feedback-fish/react';
 
 export default function FeedbackWidget({ userEmail }: { userEmail?: string }) {
-  useEffect(() => {
-    console.log('Injecting input font-size style');
-    const style = document.createElement('style');
-    style.innerHTML = `
-      input, textarea, select {
-        font-size: 16px !important;
-      }
-    `;
-    document.head.appendChild(style);
 
-    return () => {
-      document.head.removeChild(style);
-    };
-  }, []);
 
   return (
     <div className="fixed bottom-16 left-1/2 z-50 transform -translate-x-1/2">
