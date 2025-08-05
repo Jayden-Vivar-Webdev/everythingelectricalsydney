b7d566bfc01114af65e9669655af8e1f3ef3d44e
Jayden-Vivar-Webdev <jaydenjv@hotmail.com>
2025-08-05 15:41:47 +1000

    Update layout.tsx
    Changes to google tag manager scripts

--- electrical-website/src/app/layout.tsx
+++ electrical-website/src/app/layout.tsx
@@ -1,10 +1,10 @@
 // import NavBar from "./components/navbar/navbar";
 import ContactWidget from "./components/widgets/contact-float";
-import '@/app/globals.css'
+import '@/app/globals.css';
+import Script from 'next/script';
 import FooterBanner from "./components/banner/nexa-banner";
 import { Analytics } from "@vercel/analytics/next"
 // import Footer from "./components/footer/footer";
-import Head from 'next/head';
 import NavBarTemp from "./components/navbar/temp";
 
 export const metadata = {
@@ -64,22 +64,20 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Head>
+      <body>
         {/* Google Tag Manager */}
-        <script
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MP2GJLD');`,
+            })(window,document,'script','dataLayer','GTM-MP2GJLD');`,
           }}
         />
-        {/* End Google Tag Manager */}
-      </Head>
-      <body
-        
-      >
+
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
