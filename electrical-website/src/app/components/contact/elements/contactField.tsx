2468f46e86ba6a7d3892a9cd52cb504057b81af3
Vivar1 <jayden.vivar1@studytafensw.edu.au>
2026-02-27 17:12:34 +1100

    added site

--- electrical-website/src/app/components/contact/elements/contactField.tsx
+++ electrical-website/src/app/components/contact/elements/contactField.tsx
@@ -164,7 +164,7 @@ export default function ContactFormInputs({
           </div>
 
           {/* Second Row - Service Type and Description */}
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 mt-4 md:mt-6">
             <div>
               <select
                 name="serviceType"
